83ca26a9e4dc6c386853ed0b227843d8dabff978
courage331 <courage331@gmail.com>
2022-10-31 18:49:42 +0900

    Initial commit

---
+++ src/main/java/com/example/domestic301/Domestic301Application.java
@@ -0,0 +1,13 @@
+package com.example.domestic301;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Domestic301Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Domestic301Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/domestic301/Domestic301ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.domestic301;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Domestic301ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
